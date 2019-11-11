327ee25503a098abbba5e706fc68de8c3f521752
kay <ka7005te@gmail.com>
2019-11-12 00:31:19 +0800

    initial commit

--- run_jmeter.sh
+++ run_jmeter.sh
@@ -12,3 +12,8 @@ sed -i '1d' result.txt
 cat result.txt| awk   '{printf "%s%s\t%s\t%s\n", $1,$2,$3,$4}'>allwebtest.txt
 head1=$(head -1 *jtl|awk  -F "," '{printf "%s\t%s\t%s\n" ,$6,$8,$14}')
 sed "1 i$head1" -i allwebtest.txt
+
+#push to remote repo
+git commit -m "initial commit"
+git push origin master
+
