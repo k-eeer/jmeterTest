83fb74eb7170b4ed7f893ad22f5bc93776bc8d87
kay <ka7005te@gmail.com>
2019-12-13 14:49:02 +0800

    initial commit

--- run_jmeter.sh
+++ run_jmeter.sh
@@ -4,11 +4,11 @@
 rm *jtl result.txt allwebtest.txt
 
 #run jemter in non-GUI mode
-jmeter -nt allwebtest.jmx -l allwebtest.jtl -j allwebtest.log
+/home/kate/apache-jmeter-5.1.1/bin/jmeter -nt allwebtest.jmx -l allwebtest.jtl -j allwebtest.log
 
 #extract threadName, success and URL of the result
-cat allwebtest.jtl| awk  -F "," '{print $6,$8,$14}'>result.txt
-sed -i '1d' result.txt
+cat allwebtest.jtl|grep -v stress |awk  -F "," '{print $6,$8,$14}'>result.txt
+sed -i -e  '1d' result.txt
 cat result.txt| awk   '{printf "%s%s\t%s\t%s\n", $1,$2,$3,$4}'>allwebtest.txt
 head1=$(head -1 *jtl|awk  -F "," '{printf "%s\t%s\t%s\n" ,$6,$8,$14}')
 sed "1 i$head1" -i allwebtest.txt
@@ -19,5 +19,3 @@ git add .
 git pull origin master
 git commit -m "initial commit"
 git push -u origin master
-
-
